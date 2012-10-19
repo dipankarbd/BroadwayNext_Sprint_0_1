c0a5d127b088a7f72db42aca686769a62b36c70d
Chowdhury M Rahman <CMRahman@gmail.com>
2012-10-19 17:18:40 +0600

    Un Comment KO Binding for Termination

--- BroadwayNext_Sprint_0_1/Scripts/app/main.js
+++ BroadwayNext_Sprint_0_1/Scripts/app/main.js
@@ -5,4 +5,4 @@
 ko.applyBindings(bn.vmVendorList);
 ko.applyBindings(bn.vmContactList, document.getElementById("tabs-6"));
 ko.applyBindings(bn.vmShipToList, document.getElementById("tabs-7"));
-//ko.applyBindings(bn.vmTerminationList, document.getElementById("tabs-11"));
+ko.applyBindings(bn.vmTerminationList, document.getElementById("tabs-11"));
